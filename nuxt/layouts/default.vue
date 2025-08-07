5881268bf03ae1aae24accce17959b24d243262d
Mitchell Barton <mitchellcliftonbarton@gmail.com>
2025-08-07 11:39:29 -0600

    updates

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,46 +1,110 @@
 <template>
   <div id="root">
-    <p>Hello</p>
+    <MainNav />
+
+    <MobileMenu />
+
+    <main>
+      <slot />
+    </main>
+
+    <MainFooter />
+
+    <div
+      class="custom-cursor"
+      :class="{ active: showCustomCursor }"
+      :style="{ transform: `translate(${customCursorX - 60}px, ${customCursorY - 35}px)` }"
+    >
+      <p class="def-button">View Project</p>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { getGlobalData } from '~/queries/global'
+import MainNav from '~/components/MainNav.vue'
+import MainFooter from '~/components/MainFooter.vue'
+import MobileMenu from '~/components/MobileMenu.vue'
+
+// get runtime config and event bus
 const runTimeConfig = useRuntimeConfig()
 
-onMounted(async () => {
-  console.log('mounted')
+// get event bus
+const { $listen } = useNuxtApp()
 
-  // run test to see if the graphql endpoint is working
-  setTimeout(async () => {
-    const response = await fetch(runTimeConfig.public.graphqlEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-        {
-          global {
-            globalData {
-              metaDescription
-              ogImage {
-                node {
-                  mediaDetails {
-                    filePath
-                  }
-                }
-              }
-            }
-          }
-        }
-      `,
-      }),
+// fetch global data
+const { data, error } = await useAsyncData('global', () => getGlobalData({ runTimeConfig }))
+
+// destructure data
+const { projectServiceCategories } = data?.value || []
+const { footerTextSub, footerTextLarge, footerLinks } = data?.value?.global?.globalData || {}
+const { posts } = data?.value || {}
+
+const isLargeQuery = useState('isLargeQuery', () => false)
+
+// computed
+const serviceCategories = useState('serviceCategories', () => {
+  if (projectServiceCategories?.nodes?.length > 0) {
+    return projectServiceCategories.nodes
+  }
+
+  return false
+})
+
+const footerData = useState('footerData', () => {
+  return {
+    footerTextSub,
+    footerTextLarge,
+    footerLinks,
+  }
+})
+
+const allPosts = useState('allPosts', () => {
+  if (posts?.nodes?.length > 0) {
+    return posts.nodes
+  }
+
+  return false
+})
+
+// set default meta data global state
+const defaultMeta = useState('defaultMeta', () => {
+  return {
+    title: 'Abbr. Projects',
+    description: data?.value?.global?.globalData?.metaDescription ?? null,
+    ogImage: data?.value?.global?.globalData?.ogImage?.node?.mediaItemUrl ?? null,
+    ogType: 'website',
+  }
+})
+
+const mobileMenuOpen = useState('mobileMenuOpen', () => false)
+
+// custom cursor global state
+const showCustomCursor = useState('showCustomCursor', () => false)
+const customCursorX = useState('customCursorX', () => 0)
+const customCursorY = useState('customCursorY', () => 0)
+
+// listen for show close cursor event
+$listen('show-custom-cursor', () => {
+  showCustomCursor.value = true
+})
+
+// listen for hide close cursor event
+$listen('hide-custom-cursor', () => {
+  showCustomCursor.value = false
+})
+
+onMounted(() => {
+  // check if the user is on a mobile device
+  isLargeQuery.value = window.matchMedia('(min-width: 1024px)').matches
+
+  // listen for mousemove event
+  if (isLargeQuery.value) {
+    window.addEventListener('mousemove', (event) => {
+      customCursorX.value = event.clientX
+      customCursorY.value = event.clientY
     })
-
-    const data = await response.json()
-
-    console.log('layout data', data)
-  }, 1000)
+  }
 })
 </script>
 
