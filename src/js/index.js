680b2a01b6e54a2c1e40d71f65606bfcdca21159
Mitchell Barton <mitchellcliftonbarton@gmail.com>
2026-07-06 11:41:05 -0600

    log update

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   ----------------
   */
 
-  console.log('%c \nDevelopment by Cold Rice \n \ncold-rice.info \n \n', 'color: grey')
+  console.log('%c \nDevelopment by Cold Rice LLC \n \ncold-rice.info \n \n', 'color: grey')
 
   /*
   ----------------
